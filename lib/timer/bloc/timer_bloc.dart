bfdefbb80aebdffa1ed5da42448c6b73d67995ba
unknown <khandanamir7@gmail.com>
2024-12-25 19:07:17 +0330

    TimerStarted and _TimerTicked events handled

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -1,12 +1,46 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_timer/ticker.dart';
 
 part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  // TODO: set initial state
-  TimerBloc() : super() {
-    // TODO: implement event handlers
+  final Ticker _ticker;
+  static const int _duration = 60;
+
+  StreamSubscription<int>? _tickerSubscription;
+
+  // TimerBloc constructor, passes the initial state to the parent class [Bloc].
+  // If the [TimerBloc] recieves a [TimerStarted] event, it will call the _onStarted method.
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+    on<_TimerTicked>(_onTicked);
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  /// Pushes a [TimerRunInProgress] state with the start duration. If there is an open [_tickerSubscription], it cancels it.
+  /// Lastly, it listesns to the [_ticker.tick] stream and on every tick adds a [_TimerTicked] event with the remaining duration.
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: event.duration)
+        .listen((duration) => add(_TimerTicked(duration: duration)));
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(event.duration > 0
+        ? TimerRunInProgress(event.duration)
+        : TimerRunComplete());
   }
 }
