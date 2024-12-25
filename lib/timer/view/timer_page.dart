4caceeaa4baf361825f927ea5b3ac444a92aa6f0
unknown <khandanamir7@gmail.com>
2024-12-25 20:57:20 +0330

    TimerPage class  done

---
+++ lib/timer/view/timer_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/ticker.dart';
+import 'package:flutter_timer/timer/bloc/timer_bloc.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
