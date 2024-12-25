8534b2d0a79d9006a98babadf015063171be800b
unknown <khandanamir7@gmail.com>
2024-12-25 18:24:32 +0330

    .\.vscode\settings.json
    cmake directory

---
+++ lib/timer/bloc/timer_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'timer_event.dart';
+part 'timer_state.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  // TODO: set initial state
+  TimerBloc() : super() {
+    // TODO: implement event handlers
+  }
+}
